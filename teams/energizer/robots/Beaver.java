314a511a9aaa841f72e4eda88d23fbcb0a9a2d86
Merlyn <curious.attempt.bunny@gmail.com>
2015-01-18 14:47:05 -0800

    Move away from others when congested. Buildwhen uncongested.

--- teams/energizer/robots/Beaver.java
+++ teams/energizer/robots/Beaver.java
@@ -8,6 +8,8 @@ import battlecode.common.*;
 public class Beaver extends BaseRobot {
 
     private Direction planDirection;
+    private RobotInfo[] nearby;
+    private boolean congested;
 
     public Beaver(RobotController _rc) {
         super(_rc);
@@ -15,15 +17,79 @@ public class Beaver extends BaseRobot {
 
     @Override
     protected void act() throws GameActionException {
-        planMove();
-        if (rc.isCoreReady() && planDirection != null) {
-            tryMove(planDirection);
+        if (rc.isCoreReady()) {
+            nearby = rc.senseNearbyRobots(GameConstants.SUPPLY_TRANSFER_RADIUS_SQUARED, myTeam);
+
+            congested = isCongested();
+
+            if (!congested) {
+                if (rc.getTeamOre() >= RobotType.MINERFACTORY.oreCost && countOf(RobotType.MINERFACTORY) == 0) {
+                    tryBuild(planDirection.rotateLeft().rotateLeft(), RobotType.MINERFACTORY);
+                } else if (rc.getTeamOre() >= RobotType.BARRACKS.oreCost && countOf(RobotType.BARRACKS) == 0) {
+                    tryBuild(planDirection.rotateLeft().rotateLeft(), RobotType.BARRACKS);
+                } else if (rc.getTeamOre() >= RobotType.TANKFACTORY.oreCost && countOf(RobotType.TANKFACTORY) == 0) {
+                    tryBuild(planDirection.rotateLeft().rotateLeft(), RobotType.TANKFACTORY);
+                }
+            }
+            if (congested) {
+                planMoveAway();
+            } else {
+                planDirection = facing;
+            }
+
+            if (planDirection != null) {
+                tryMove(planDirection);
+                if (rc.isCoreReady()) {
+                    tryMove(planDirection.rotateRight().rotateRight());
+                }
+            }
         }
     }
 
-    private void planMove() {
-        RobotInfo[] nearby = rc.senseNearbyRobots(GameConstants.SUPPLY_TRANSFER_RADIUS_SQUARED, myTeam);
+    private boolean isCongested() {
+//        int xMin = rc.getLocation().x - 4;
+//        int xMax = rc.getLocation().x + 4;
+//        int yMin = rc.getLocation().y - 4;
+//        int yMax = rc.getLocation().y + 4;
+//        int obstructed = 0;
+//
+//        for(int x=xMin; x<=xMax; x++) {
+//            for(int y=yMin; y<=yMax; y++) {
+//                TerrainTile terrainTile = rc.senseTerrainTile(new MapLocation(x, y));
+//                if (!terrainTile.isTraversable()) {
+//                    obstructed++;
+//                }
+//            }
+//        }
+//
+//        double ratio = obstructed / (9.0*9.0);
+//        rc.setIndicatorString(1, "Obstruction ratio is "+ratio);
+//
+//        return ratio > 0.25;
 
+        int clearCount = 0;
+        for(Direction d : directions) {
+            MapLocation target = rc.getLocation();
+            boolean clear = true;
+            for(int extent = 0; extent<5; extent++) {
+                target = target.add(d);
+                TerrainTile terrainTile = rc.senseTerrainTile(target);
+                if (!terrainTile.isTraversable()) {
+                    clear = false;
+                    break;
+                }
+            }
+            if (clear) {
+                clearCount++;
+            }
+        }
+
+        rc.setIndicatorString(1, "Obstruction: clear count = "+clearCount+". Congested? = "+(clearCount <= 3));
+
+        return clearCount < 3;
+    }
+
+    private void planMoveAway() {
         int sumX = 0;
         int sumY = 0;
         double count = 0;
@@ -37,9 +103,13 @@ public class Beaver extends BaseRobot {
         double x = -(sumX / count);
         double y = -(sumY / count);
 
-        planDirection = vectorToDirection(x,y);
+        if (count > 0) {
+            planDirection = vectorToDirection(x,y);
+        } else {
+//            planDirection = null;
+        }
 
-        rc.setIndicatorString(0, planDirection+" from vector "+x+","+y);
+        rc.setIndicatorString(0, planDirection + " from vector " + x + "," + y);
     }
 
     private Direction vectorToDirection(double x, double y) {
--- teams/energizer/robots/HQ.java
+++ teams/energizer/robots/HQ.java
@@ -12,15 +12,15 @@ public class HQ extends BaseRobot {
 
     @Override
     protected void act() throws GameActionException {
-//        RobotInfo[] myRobots = rc.senseNearbyRobots(999999, myTeam);
-//        int[] counts = new int[RobotType.MISSILE.ordinal()+1];
-//        for (RobotInfo r : myRobots) {
-//            counts[r.type.ordinal()]++;
-//        }
-//        for(RobotType type : RobotType.values()) {
-//            rc.broadcast(type.ordinal(), counts[type.ordinal()]);
-//        }
-        if (rc.isCoreReady() && rc.getTeamOre() >= RobotType.BEAVER.oreCost) {
+        RobotInfo[] myRobots = rc.senseNearbyRobots(999999, myTeam);
+        int[] counts = new int[RobotType.MISSILE.ordinal()+1];
+        for (RobotInfo r : myRobots) {
+            counts[r.type.ordinal()]++;
+        }
+        for(RobotType type : RobotType.values()) {
+            rc.broadcast(type.ordinal(), counts[type.ordinal()]);
+        }
+        if (rc.isCoreReady() && rc.getTeamOre() >= RobotType.BEAVER.oreCost && countOf(RobotType.BEAVER) < 10) {
             trySpawn(Direction.NORTH, RobotType.BEAVER);
         }
     }
