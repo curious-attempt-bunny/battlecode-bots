1ea60a24f39c52f74629cda81da1ab2ba90f6056
Merlyn <curious.attempt.bunny@gmail.com>
2015-01-24 23:24:27 -0800

    Only build on supply boundaries.

--- teams/expando/robots/Beaver.java
+++ teams/expando/robots/Beaver.java
@@ -12,7 +12,6 @@ import java.util.Set;
 public class Beaver extends BaseRobot {
 
     private final CoordinateSystem coordinateSystem;
-    private final Set<MapLocation> builtLocations = new HashSet<MapLocation>();
 
     public Beaver(RobotController _rc) {
         super(_rc);
@@ -28,49 +27,51 @@ public class Beaver extends BaseRobot {
 
         if (rc.isCoreReady()) {
             Direction buildDirection = getBuildDirection();
-            int relayCount = relayCount();
-            if (relayCount == 1) {
-                if (rc.getTeamOre() >= RobotType.TANKFACTORY.oreCost && rc.hasBuildRequirements(RobotType.TANKFACTORY) &&
-                        (countOf(RobotType.TANKFACTORY) == 0 || rc.getTeamOre() > Math.min(3000, 1500*countOf(RobotType.TANKFACTORY))) &&
-                        (!isCongested() || rc.getTeamOre() > 2000)) {
-                    doBuild(buildDirection, RobotType.TANKFACTORY);
-                }
-            } else if (relayCount == 0) {
-                if (buildDirection != null) {
-
-                    boolean built = false;
-                    if (countOf(RobotType.MINERFACTORY) == 0) {
-                        if (rc.getTeamOre() >= RobotType.MINERFACTORY.oreCost) {
-    //                        Direction bestOrdeBuildDirection = bestOreBuildDirection();
-    //                        if (bestOrdeBuildDirection != null) {
-    //                            buildDirection = bestOrdeBuildDirection;
-    //                        }
-                            built = tryBuild(buildDirection, RobotType.MINERFACTORY);
-                        }
-                    } else if (countOf(RobotType.BARRACKS) == 0) {
-                        if (rc.getTeamOre() >= RobotType.BARRACKS.oreCost) {
-                            built = doBuild(buildDirection, RobotType.BARRACKS);
-                        }
-                    } else if (countOf(RobotType.TANK) == 0 || countOfNearbyFriendly(RobotType.TANK, 2 * GameConstants.SUPPLY_TRANSFER_RADIUS_SQUARED) > 0 || 2*rc.getLocation().distanceSquaredTo(rc.senseEnemyHQLocation()) > 3*rc.getLocation().distanceSquaredTo(rc.senseHQLocation())) {
-                        if (rc.getTeamOre() >= RobotType.TANKFACTORY.oreCost && rc.hasBuildRequirements(RobotType.TANKFACTORY) &&
-                                (countOf(RobotType.TANKFACTORY) == 0 || rc.getTeamOre() > Math.min(3000, 1500*countOf(RobotType.TANKFACTORY))) &&
-                                (!isCongested() || rc.getTeamOre() > 2000)) {
-                            built = doBuild(buildDirection, RobotType.TANKFACTORY);
-//                        } else if (rc.getTeamOre() >= RobotType.TECHNOLOGYINSTITUTE.oreCost && (congested || countOf(RobotType.TANK) > 0) && countOfNearbyFriendly(RobotType.TECHNOLOGYINSTITUTE, 20*20) == 0) {
-//                            built = doBuild(buildDirection, RobotType.TECHNOLOGYINSTITUTE);
-                        } else if (rc.getTeamOre() >= RobotType.SUPPLYDEPOT.oreCost) { // && 5*countOf(RobotType.SUPPLYDEPOT) < 4*countOf(RobotType.COMPUTER)) {
-                            built = doBuild(buildDirection, RobotType.SUPPLYDEPOT);
-                        }
+            if (buildDirection != null) {
+                int relayCount = relayCount();
+                if (relayCount == 1) {
+                    if (rc.getTeamOre() >= RobotType.TANKFACTORY.oreCost && rc.hasBuildRequirements(RobotType.TANKFACTORY) &&
+                            (countOf(RobotType.TANKFACTORY) == 0 || rc.getTeamOre() > Math.min(3000, 1500*countOf(RobotType.TANKFACTORY))) &&
+                            (!isCongested() || rc.getTeamOre() > 2000)) {
+                        doBuild(buildDirection, RobotType.TANKFACTORY);
                     }
+                } else if (relayCount == 0) {
+                    if (buildDirection != null) {
 
-                    if (rc.isCoreReady()) {
-                        mineAndTrack();
-                    }
+                        boolean built = false;
+                        if (countOf(RobotType.MINERFACTORY) == 0) {
+                            if (rc.getTeamOre() >= RobotType.MINERFACTORY.oreCost) {
+        //                        Direction bestOrdeBuildDirection = bestOreBuildDirection();
+        //                        if (bestOrdeBuildDirection != null) {
+        //                            buildDirection = bestOrdeBuildDirection;
+        //                        }
+                                built = tryBuild(buildDirection, RobotType.MINERFACTORY);
+                            }
+                        } else if (countOf(RobotType.BARRACKS) == 0) {
+                            if (rc.getTeamOre() >= RobotType.BARRACKS.oreCost) {
+                                built = doBuild(buildDirection, RobotType.BARRACKS);
+                            }
+                        } else if (countOf(RobotType.TANK) == 0 || countOfNearbyFriendly(RobotType.TANK, 2 * GameConstants.SUPPLY_TRANSFER_RADIUS_SQUARED) > 0 || 2*rc.getLocation().distanceSquaredTo(rc.senseEnemyHQLocation()) > 3*rc.getLocation().distanceSquaredTo(rc.senseHQLocation())) {
+                            if (rc.getTeamOre() >= RobotType.TANKFACTORY.oreCost && rc.hasBuildRequirements(RobotType.TANKFACTORY) &&
+                                    (countOf(RobotType.TANKFACTORY) == 0 || rc.getTeamOre() > Math.min(3000, 1500*countOf(RobotType.TANKFACTORY))) &&
+                                    (!isCongested() || rc.getTeamOre() > 2000)) {
+                                built = doBuild(buildDirection, RobotType.TANKFACTORY);
+    //                        } else if (rc.getTeamOre() >= RobotType.TECHNOLOGYINSTITUTE.oreCost && (congested || countOf(RobotType.TANK) > 0) && countOfNearbyFriendly(RobotType.TECHNOLOGYINSTITUTE, 20*20) == 0) {
+    //                            built = doBuild(buildDirection, RobotType.TECHNOLOGYINSTITUTE);
+                            } else if (rc.getTeamOre() >= RobotType.SUPPLYDEPOT.oreCost) { // && 5*countOf(RobotType.SUPPLYDEPOT) < 4*countOf(RobotType.COMPUTER)) {
+                                built = doBuild(buildDirection, RobotType.SUPPLYDEPOT);
+                            }
+                        }
 
-                    if (built || (rand.nextInt(15) == 0 && rc.getSupplyLevel() < 100)) {
-                        facing = rc.getLocation().directionTo(rc.senseHQLocation());
+                        if (rc.isCoreReady()) {
+                            mineAndTrack();
+                        }
+
+                        if (built || (rand.nextInt(15) == 0 && rc.getSupplyLevel() < 100)) {
+                            facing = rc.getLocation().directionTo(rc.senseHQLocation());
+                        }
+                        return;
                     }
-                    return;
                 }
             }
 
@@ -106,12 +107,26 @@ public class Beaver extends BaseRobot {
 
     private boolean doBuild(Direction buildDirection, RobotType type) throws GameActionException {
         rc.build(buildDirection, type);
-        builtLocations.add(rc.getLocation().add(buildDirection));
         return true;
     }
 
-    private Direction getBuildDirection() {
-        return facing.rotateRight().rotateRight().rotateRight();
+//    private Direction getBuildDirection() {
+//        return facing.rotateRight().rotateRight().rotateRight();
+//    }
+
+    private Direction getBuildDirection() throws GameActionException {
+        MapLocation normalizedLocation = coordinateSystem.toNormalized(rc.getLocation());
+        Direction direction = null;
+
+        for(Direction d : directions) {
+            MapLocation location = normalizedLocation.add(d);
+            if (rc.readBroadcast(200 + coordinateSystem.broadcastOffsetForNormalizated(location)) == 1) {
+                direction = d;
+                break;
+            }
+        }
+
+        return direction;
     }
 }
 
